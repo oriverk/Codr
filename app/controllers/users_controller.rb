72ba9579d4fc4aef115fd546643a02d69f063698
oriverk <se.sterroristalqaeda@gmail.com>
2019-11-03 16:01:11 +0000

    rails g controller users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  end
+
+  # deviseで処理するので不要
+  # def new
+  # end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
