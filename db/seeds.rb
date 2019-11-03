0f04e16fde03fae9eaae0be9265444966dd0b1ff
oriverk <se.sterroristalqaeda@gmail.com>
2019-11-03 22:46:07 +0000

    udpate post views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # admin
 User.create(name:"oriverk", email:"se.sterroristalqaeda@gmail.com", password:"riverfield", admin:true)
+(1..4).each do |m|
+  Post.create(user_id:1,name:"admin-post",content:('a'..'z').to_a.shuffle[1..4].join.to_s)
+end
 
 # general
 # make account
@@ -7,17 +10,13 @@ User.create(name:"oriverk", email:"se.sterroristalqaeda@gmail.com", password:"ri
   nm = (1..4).map { ('あ'..'ん').to_a[rand(20)] }.join
   em = ('a'..'z').to_a.shuffle[1..4].join
   User.create!(name:"#{nm}", email:"#{em}#{i}@gmail.com",password: SecureRandom.base64)
-end
-
-# make post
+  # make post
 # 一人ずつ3ポスト
-  # (1..6).each do |m|
-  #   (1..3).each do |s|
-  #     postNm = (1..5).map { ('あ'..'ん').to_a[rand(20)] }.join
-  #     postEm = ('A'..'Z').to_a.shuffle[1..8].join 
-  #     Post.create!(user:"#{m}",name:"#{postNm}", content:"#{postEm}", date:Time.now)
-  #   end
-  # end
-
+  (1..3).each do |s|
+    postNm = (1..5).map { ('あ'..'ん').to_a[rand(20)] }.join
+    postEm = ('A'..'Z').to_a.shuffle[1..8].join 
+    Post.create!(user_id:i+1,name:postNm.to_s, content:postEm.to_s, date:Time.now)
+  end
+end
   
 
