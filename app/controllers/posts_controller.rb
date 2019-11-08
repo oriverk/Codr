b9bfb1d1ab3ee64be3075541566086948baa0cc6
oriverk <se.sterroristalqaeda@gmail.com>
2019-11-08 07:41:14 +0000

    add html2canvas.js

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,8 +34,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
-        format.json { render :index, status: :created, location: @posts }
+        format.html { redirect_to post_path(@post), notice: 'Post was successfully created.' }
+        format.json { render :index, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -48,8 +48,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @posts }
+        format.html { redirect_to post_path, notice: 'Post was successfully updated.' }
+        format.json { render :index, status: :ok, location: post }
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
